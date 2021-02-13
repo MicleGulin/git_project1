3e3be70e73f43a687a367c0f1a570e80de59a7dc
MicleGulin <73758419+MicleGulin@users.noreply.github.com>
2021-02-14 00:08:54 +0300

    Update program.py

--- program.py
+++ program.py
@@ -1,9 +1,13 @@
-﻿def main():
-    print('My first git program')
-    print('And I change it every day')
-    print('Again')
-    print('UFO came and added this line')
+    # Рамка вокруг объекта:
+    envelope = toponym["boundedBy"]["Envelope"]
 
+    # левая, нижняя, правая и верхняя границы из координат углов:
+    l, b = envelope["lowerCorner"].split(" ")
+    r, t = envelope["upperCorner"].split(" ")
 
-if __name__ == '__main__':
-    main()
+    # Вычисляем полуразмеры по вертикали и горизонтали
+    dx = abs(float(l) - float(r)) / 2.0
+    dy = abs(float(t) - float(b)) / 2.0
+
+    # Собираем размеры в параметр span
+    span = f"{dx},{dy}"
